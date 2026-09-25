50ec3ecd053ca04adca4aefcf4fe87e67eb1fbe5
Joshua Bontuyan <joshuabontuyan1@gmail.com!>
2026-09-25 17:54:59 +0800

    Updated from Vehicle class with constructor

--- Vehicle.java
+++ Vehicle.java
@@ -1,15 +1,26 @@
+
 public class Vehicle {
+
     String brand;
     String model;
     int year;
 
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
     void displayInfo() {
         System.out.println(brand + " " + model + " (" + year + ")");
     }
+
     int calculateAge() {
         return 2026 - year;
     }
+
     boolean isVintage() {
         return calculateAge() > 25;
     }
+    
 }
